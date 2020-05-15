29a5bccdf0dc02d9d175ee3c29ff7ac8ec4d12ad
yangfan <yangfanyf.yang@qunar.com>
2020-05-15 15:45:00 +0800

    第二种扩展模块方式

--- src/g-extends.ts
+++ src/g-extends.ts
@@ -7,3 +7,12 @@ declare global {
 // 注意: 修改"全局声明"必须在模块内部, 所以至少要有 export{}字样
 // 不然会报错❌: 全局范围的扩大仅可直接嵌套在外部模块中或环境模块声明中
 export {}
+
+// 第二种扩展模块方式
+// declare namespace NodeJS {
+//     interface Global {
+//         abc: string;
+//     }
+// }
+
+// global.abc = '123';
